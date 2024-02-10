57daf466eff95877fc127c5e286b2ee76df79838
Flesh <olivia.a.gowan@gmail.com>
2024-02-10 14:31:45 -0500

    Product.js

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,38 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    Id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    Product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    Price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    Stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    Category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'Id',
+      },
+    },
   },
   {
     sequelize,
