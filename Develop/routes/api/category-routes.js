aff51e869149a589cf16a53f1bff06fafc732a15
olivia_gowan <olivia.a.gowan@gmail.com>
2024-02-15 14:16:32 -0500

    created routes for the categories

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -2,6 +2,9 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
+router.get('/api/categories', async (req, res) => {
+  
+});
 
 router.get('/', (req, res) => {
   // find all categories
